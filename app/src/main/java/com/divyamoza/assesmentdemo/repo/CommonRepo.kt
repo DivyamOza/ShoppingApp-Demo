b9917202a7e865d52485a6340cb5ad28e3fe1966
Divyam Oza <divyam.oza55@gmail.com>
2022-04-24 13:04:28 +0530

    - Created Dabase CRUD operation methods into Repo and Viewmodels.

--- app/src/main/java/com/divyamoza/assesmentdemo/repo/CommonRepo.kt
+++ app/src/main/java/com/divyamoza/assesmentdemo/repo/CommonRepo.kt
@@ -4,6 +4,8 @@ import androidx.lifecycle.MutableLiveData
 import com.divyamoza.assesmentdemo.base.AssesmentDemoApp
 import com.divyamoza.assesmentdemo.data.ApiClient
 import com.divyamoza.assesmentdemo.data.Resource
+import com.divyamoza.assesmentdemo.models.dbentity.Gadget
+import com.divyamoza.assesmentdemo.models.dbentity.GadgetDatabase
 import com.divyamoza.assesmentdemo.models.responses.CommonResponse
 import com.divyamoza.assesmentdemo.models.responses.GadgetsResponse
 import com.divyamoza.assesmentdemo.ui.activities.HomeActivity
@@ -20,13 +22,25 @@ import timber.log.Timber
  * @constructor Create empty Common repo
  */
 class CommonRepo {
+    var database: GadgetDatabase = GadgetDatabase.getDatabase(context = AssesmentDemoApp.context)
     private var gadgetsResponse: MutableLiveData<Resource<GadgetsResponse>> = MutableLiveData()
+    private var gadgetsCount: MutableLiveData<Int?> = MutableLiveData()
+    private var getAllGadgets: MutableLiveData<List<Gadget?>>? = MutableLiveData()
 
 
     init {
         gadgetsResponse.value = Resource.loading(null)
+        gadgetsCount.value = 0
+        getAllGadgets?.value = listOf()
     }
 
+
+    /**
+     * Get gadgets
+     *
+     * @param activity
+     * @return
+     */
     suspend fun getGadgets(activity: HomeActivity): MutableLiveData<Resource<GadgetsResponse>> {
         if (NetworkUtils.isNetworkAvailable(AssesmentDemoApp.context)) {
             val response = withContext(Dispatchers.IO) {
@@ -53,5 +67,76 @@ class CommonRepo {
             Timber.e("No Internet!!!")
         }
         return gadgetsResponse
+    }
+
+    //----------------------------//
+    // Database Operations Methods
+    //----------------------------//
+    
+    /**
+     * Add gadget into DB
+     *
+     * @param gadget
+     */
+    suspend fun addGadgetIntoDB(gadget: Gadget) {
+        withContext(Dispatchers.IO) {
+            database.gadgetDao().addGadget(gadget)
+        }
+    }
+
+
+    /**
+     * Delete gadget from DB
+     *
+     * @param gadget
+     */
+    suspend fun deleteGadgetFromDB(gadget: Gadget) {
+        withContext(Dispatchers.IO) {
+            database.gadgetDao().deleteGadget(gadget)
+        }
+    }
+
+
+    /**
+     * Update gadget into DB
+     *
+     * @param gadget
+     */
+    suspend fun updateGadgetIntoDB(gadget: Gadget) {
+        withContext(Dispatchers.IO) {
+            database.gadgetDao().updateGadget(gadget)
+        }
+    }
+
+
+    /**
+     * Get all gadgets from DB
+     *
+     * @return
+     */
+    suspend fun getAllGadgetsFromDB(): MutableLiveData<List<Gadget?>>? {
+        val response = withContext(Dispatchers.IO) {
+            database.gadgetDao().getAllGadgets()
+        }
+        response?.let {
+            getAllGadgets?.value = it.value
+        }
+        return getAllGadgets
+    }
+
+
+    /**
+     * Get gadgets counts from DB
+     *
+     * @return
+     */
+    suspend fun getGadgetsCountsFromDB(): MutableLiveData<Int?> {
+        val response = withContext(Dispatchers.IO) {
+            database.gadgetDao().getGadgetsCount()
+        }
+        response?.let {
+            gadgetsCount.value = it.value
+        }
+        return gadgetsCount
     }
 }
--- app/src/main/java/com/divyamoza/assesmentdemo/viewmodels/CommonViewModel.kt
+++ app/src/main/java/com/divyamoza/assesmentdemo/viewmodels/CommonViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.divyamoza.assesmentdemo.data.Resource
+import com.divyamoza.assesmentdemo.models.dbentity.Gadget
 import com.divyamoza.assesmentdemo.models.responses.GadgetsResponse
 import com.divyamoza.assesmentdemo.repo.CommonRepo
 import com.divyamoza.assesmentdemo.ui.activities.HomeActivity
@@ -16,9 +17,11 @@ import kotlinx.coroutines.launch
  * @constructor Create empty Common view model
  */
 class CommonViewModel : ViewModel() {
+    private val commonRepo: CommonRepo = CommonRepo()
     var gadgetsResponse: MutableLiveData<Resource<GadgetsResponse>> =
         MutableLiveData()
-    private val commonRepo: CommonRepo = CommonRepo()
+    var gadgetsCountResponse: MutableLiveData<Int?> = MutableLiveData()
+    var getAllGadgetsResponse: MutableLiveData<List<Gadget?>>? = MutableLiveData()
 
 
     /**
@@ -31,6 +34,68 @@ class CommonViewModel : ViewModel() {
             gadgetsResponse.value = Resource.loading(null)
             gadgetsResponse.value =
                 commonRepo.getGadgets(activity = activity).value
+        }
+    }
+
+    //----------------------------//
+    // Database Operations Methods
+    //----------------------------//
+
+
+    /**
+     * Add gadget into DB
+     *
+     * @param gadget
+     */
+    fun addGadgetIntoDB(gadget: Gadget) {
+        viewModelScope.launch {
+            commonRepo.addGadgetIntoDB(gadget = gadget)
+        }
+    }
+
+    /**
+     * Update gadget into DB
+     *
+     * @param gadget
+     */
+    fun updateGadgetIntoDB(gadget: Gadget) {
+        viewModelScope.launch {
+            commonRepo.updateGadgetIntoDB(gadget = gadget)
+        }
+    }
+
+    /**
+     * Delete gadget from DB
+     *
+     * @param gadget
+     */
+    fun deleteGadgetFromDB(gadget: Gadget) {
+        viewModelScope.launch {
+            commonRepo.deleteGadgetFromDB(gadget = gadget)
+        }
+    }
+
+    /**
+     * Get all gadgets from DB
+     *
+     */
+    fun getAllGadgetsFromDB() {
+        viewModelScope.launch {
+            getAllGadgetsResponse?.value = listOf()
+            getAllGadgetsResponse?.value =
+                commonRepo.getAllGadgetsFromDB()?.value
+        }
+    }
+
+    /**
+     * Get gadgets counts from DB
+     *
+     */
+    fun getGadgetsCountsFromDB() {
+        viewModelScope.launch {
+            gadgetsCountResponse.value = 0
+            gadgetsCountResponse.value =
+                commonRepo.getGadgetsCountsFromDB().value
         }
     }
 }
