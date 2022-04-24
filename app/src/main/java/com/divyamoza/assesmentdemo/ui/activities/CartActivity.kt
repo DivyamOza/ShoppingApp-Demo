2e408b2063bc3e0601aeb4fb33b4d1997d4f3082
Divyam Oza <divyam.oza55@gmail.com>
2022-04-24 12:53:31 +0530

    -  Added Cart Screen designs.

---
+++ app/src/main/java/com/divyamoza/assesmentdemo/ui/activities/CartActivity.kt
@@ -0,0 +1,12 @@
+package com.divyamoza.assesmentdemo.ui.activities
+
+import android.os.Bundle
+import com.divyamoza.assesmentdemo.R
+import com.divyamoza.assesmentdemo.base.BaseActivity
+
+class CartActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cart)
+    }
+}
